9e8ca9ef1494cfe73e128c76d495d839262dd552
Mica Horton <mica.e.horton@gmail.com>
2020-08-19 16:22:08 -0700

    Change app_start file

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/server/src
 npm start
-pm2 start npm --name "hyfix-website" -- start
+NODE_PORT=80 pm2 start npm --name "hyfix-website" -- start
 pm2 startup
 pm2 save
 pm2 restart all
