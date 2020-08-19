61e2acff6c9c24cfba810d9c390789f65ddd679e
Mica Horton <mica.e.horton@gmail.com>
2020-08-19 12:50:27 -0700

    Remove yum from before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/server
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+apt-get -y install nodejs npm
+#yum -y install nodejs npm
