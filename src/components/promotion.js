544cf182894e579900401f61f5f914ff862febe3
Mica Horton <mica.e.horton@gmail.com>
2020-08-25 12:12:55 -0700

    Fix the scroll problem

--- src/components/promotion.js
+++ src/components/promotion.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <iframe src='https://docs.google.com/forms/d/e/1FAIpQLScDjQ5MfYBdhCLlm5o4Axd6HpqVfNAkZWQquKbI2B9I0WLBlA/viewform?embedded=true' width='640' height='943' frameborder='0'scrolling='no' marginheight='0' marginwidth='0' >Loading…</iframe>      
+      <iframe src='https://docs.google.com/forms/d/e/1FAIpQLScDjQ5MfYBdhCLlm5o4Axd6HpqVfNAkZWQquKbI2B9I0WLBlA/viewform?embedded=true' width='640' height='1500' frameborder='0'scrolling='no' marginheight='0' marginwidth='0' >Loading…</iframe>      
     );
   }
 }
