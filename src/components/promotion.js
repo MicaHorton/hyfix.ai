1f502bf07d91e9f9f293f8c7c2b501b86242c0da
Mica Horton <mica.e.horton@gmail.com>
2020-08-25 21:24:06 -0700

    Add SSL certificate
    Files not actually included of course.

--- src/components/promotion.js
+++ src/components/promotion.js
@@ -13,3 +13,8 @@ export default class About extends Component {
 /*
 <iframe src='https://docs.google.com/forms/d/e/1FAIpQLScDjQ5MfYBdhCLlm5o4Axd6HpqVfNAkZWQquKbI2B9I0WLBlA/viewform?embedded=true' width='640' height='943' frameborder='0'  scrolling='no'>Loading…</iframe>
 */
+
+/*
+Make your roof work for you!
+Make your roof work for you! Get paid yearly to support science by hosting a GPS base station on your roof.
+*/
