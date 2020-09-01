00241fb00575b3908ea48dda0115327346b5c788
Mica Horton <mica.e.horton@gmail.com>
2020-09-01 16:46:51 -0700

    Make more mobile friendly

--- src/components/promotion.js
+++ src/components/promotion.js
@@ -7,9 +7,9 @@ export default class About extends Component {
     return (
       <div className='two-column'>
         <div className='two-column-item'>
-          <h1>Make your roof work for you.</h1>
 
           <div className='base'>
+            <h1>Make your roof work for you.</h1>
             <img className='base-image' src={base} alt='base'/>
             <p className='base-caption'>
                    Interested in making money by hosting a small space weather station?
