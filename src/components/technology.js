1176391dc1b4d21020e5ee54a2881e4a8a3e972c
Mica Horton <mica.e.horton@gmail.com>
2020-10-29 16:06:14 -0700

    Update font styling

--- src/components/technology.js
+++ src/components/technology.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import technology_banner from '../media/technology_banner.jpg';
 
 export default class Technology extends Component {
   render() {
     return (
-      <>
-        <div className='about'>
+        <div>
+          <header className='banner'>
+            <img className='banner-image' src={technology_banner} alt='logo'/>
+            <h1 className='banner-text'>Technologies<br/> Shaping the Future</h1>
+          </header>
+
           <h1>Technology</h1> 
           <p>cute icons, cute icons, more cute icons</p>
           
         
         </div>
-       
-      </>
+      
     );
   }
 }
+
+/*
+Monsterrat
+Lora
+Hindi Maduri
+https://fonts.google.com/specimen/Hind+Vadodara
+https://fonts.google.com/specimen/Hind+Vadodara
+https://fonts.google.com/specimen/Rosario
+*/
