ffc226638ec30a1eb6e3f6a3f96b5c4aec5fc5a2
Mica Horton <mica.e.horton@gmail.com>
2020-11-01 10:22:21 -0800

    Adjust according to Mike's feedback
    Close: #1

---
+++ src/components/contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component {
+  render() {
+    return (
+        <main className='contact-page'>
+            
+
+    
+            <iframe className='contact-form' src="https://docs.google.com/forms/d/e/1FAIpQLSdg_qzN2I1G6xdWoBnZdXkfmNpGrwGVjzVvmgqdKYlbAJvvOw/viewform?embedded=true" width='100%' height='1000px' scrolling="no" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+           
+
+            
+        </main>     
+    );
+  }
+}
+
