13f79c9d0d0923a6aa8a1b38cc3a5a60aa9583a3
Mica Horton <mica.e.horton@gmail.com>
2020-11-02 16:15:13 -0800

    Update contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,15 +4,28 @@ export default class Contact extends Component {
   render() {
     return (
         <main className='contact-page'>
-            
 
-    
-            <iframe className='contact-form' src="https://docs.google.com/forms/d/e/1FAIpQLSdg_qzN2I1G6xdWoBnZdXkfmNpGrwGVjzVvmgqdKYlbAJvvOw/viewform?embedded=true" width='100%' height='1000px' scrolling="no" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-           
+          <div className='contact-text'>
+            <section className='section section-light'>
+              <h2 className='section-text'>Make Your Base Station Earn Rewards for You!</h2>
+            </section>
 
+            <section className='section section-dark'> 
+              <p className='section-text'>
+                  HYFIX is building the world's largest, most modernized, community-driven RTK network. 
+                  PAS (Precision As a Service) is a blockchain based token that enables peer-to-peer networking 
+                  for GNSS data aggregation and distribution. PAS blockchain allows RTK base stations to mine PAS rewards. 
+                  By connecting your base stations into this blockchain network, you can be part of the global 
+                  network and get rewarded with PAS tokens.
+              </p>
+            </section>
+          </div>
+
+          
+
+          <iframe className='contact-form ' src="https://docs.google.com/forms/d/e/1FAIpQLSdg_qzN2I1G6xdWoBnZdXkfmNpGrwGVjzVvmgqdKYlbAJvvOw/viewform?embedded=true" width='100%' height='1000px' scrolling="no" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             
         </main>     
     );
   }
 }
-
