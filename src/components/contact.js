ab45d1978c56e5dbf7a73b0891efa73f53b57c13
Mica Horton <mica.e.horton@gmail.com>
2020-11-12 19:17:08 -0800

    Update contact page
    Close: #3

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,32 +7,16 @@ export default class Contact extends Component {
     return (
         <main className='contact-page'>
 
-          <header className='banner'>
-            <img className='banner-image' src={company_banner} alt='image of globe'/>
-            <h1 className='banner-text'>Make Your Base Station <br/> Earn Rewards for You!</h1>
-          </header>
-
-          <section className='section section-dark'> 
+          <section className='section section-light'> 
             <p className='section-text'>
-                HYFIX is building the world's largest, most modernized, community-driven RTK network. 
-                PAS (Precision As a Service) is a blockchain based token that enables peer-to-peer networking 
-                for GNSS data aggregation and distribution. PAS blockchain allows RTK base stations to mine PAS rewards. 
-                By connecting your base stations into this blockchain network, you can be part of the global 
-                network and get rewarded with PAS tokens. 
+              Receive a FREE survey-grade RTK base station and add it to the HYFIX global correction network,
+              OR earn up to $1000/yr by contributing a real-time data stream from a qualifying RTK base station.  
+              Apply Now! 
             </p>
           </section>
-
-          
-
-          <section className='example section-light'>
-            <p className='example-text'>Here's an example of what your base station could be earning for you:</p>
-            <img className='example-image' src={example} alt='statistics about bastation earnings'/>
-          </section>
-
-          
   
-          <iframe className='contact-form ' src="https://docs.google.com/forms/d/e/1FAIpQLSdg_qzN2I1G6xdWoBnZdXkfmNpGrwGVjzVvmgqdKYlbAJvvOw/viewform?embedded=true" width='100%' height='1000px' scrolling="no" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-            
+          <iframe className='contact-form ' src="https://docs.google.com/forms/d/e/1FAIpQLSeuyqyQ2DzcEfEypfq9IW0LqRQpW3GMkGuy1OpVP4Dnscf0vw/viewform?embedded=true" width='100%' height='1300px' scrolling="no" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        
         </main>     
     );
   }
