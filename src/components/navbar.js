47d964d0aac1dd8c981acd61440403d5d1c79102
Mica Horton <mica.e.horton@gmail.com>
2020-12-16 16:12:21 -0800

    Fix small typos

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../App.css';
-import logo from '../media/logo.png';
+import full_logo from '../media/full_logo.png';
+import half_logo from '../media/half_logo.png';
 /*
 import Image from '../cover-image.jpg';
 <img src={Image} alt='website header'/>
@@ -14,7 +15,8 @@ export default class Navbar extends Component {
 
           <nav className='nav'>
               <Link to='/' className='logo'>
-                <img className='logo-image' src={logo} alt='logo'/>
+                <img className='logo-image logo-web' src={full_logo} alt='logo'/>
+                <img className='logo-image logo-mobile' src={half_logo} alt='logo'/>
               </Link>
               
               <div className='menu'>
