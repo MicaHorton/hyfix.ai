55a3e773a574c1fd468d51ffa746f2a021c95866
Mica Horton <mica.e.horton@gmail.com>
2020-12-17 09:04:57 -0800

    Fix merge conflicts

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,52 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../App.css';
-import logo from '../media/logo.png';
-/*
-import Image from '../cover-image.jpg';
-<img src={Image} alt='website header'/>
-*/
-
+import logo from '../media/full_logo.png';
+import 'font-awesome/css/font-awesome.min.css';
 
 export default class Navbar extends Component {
-  render() {
-    return (
-
-          <nav className='nav'>
-              <Link to='/' className='logo'>
-                <img className='logo-image' src={logo} alt='logo'/>
-              </Link>
-              
-              <div className='menu'>
-                <Link to='/applications' className='menu-item'>Applications</Link>
-                <Link to='/network' className='menu-item'>Network</Link>
-                <Link to='/technology' className='menu-item'>Technology</Link>
-                <Link to='/contact' className='menu-item'>Contact</Link>
-              </div>
-
-
-          </nav>
-
-      
-    );
+  constructor (props) {
+    super(props);
+    this.toggleOverlay = this.toggleOverlay.bind(this);
   }
+
+  toggleOverlay() {
+    document.body.style.backgroundColor = 'white';
+  }
+
+  render() {
+  
+
+    return (
+          <nav>
+
+              <header>
+                <button className='fa fa-bars'></button>
+                <Link to='/' className='logo'>
+                  <img className='logo-image' src={logo} alt='logo'/>
+                </Link>
+              </header>
+
+              <div className='menu-mobile'>
+                <Link to='/applications' className='menu-item-mobile'>Applications</Link>
+                <Link to='/network' className='menu-item-mobile'>Network</Link>
+                <Link to='/technology' className='menu-item-mobile'>Technology</Link>
+                <Link to='/contact' className='menu-item-mobile'>Contact</Link>
+              </div>
+           
+          </nav>
+    );
+
+  }
+  
 }
 
 /*
-Jess
-Buissness
-Sports
-Transfer 4 Year College
+
+on click: 
+toggle
+  which button rendered
+  background overlay color
+  which menu style to use
+<img className='logo-image logo-mobile' src={half_logo} alt='logo'/>
 */
