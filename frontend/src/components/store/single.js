91a85edafaf89c0a5a23cc49fd87eab97bfa16ec
Mica Horton <mica.e.horton@gmail.com>
2020-12-19 18:53:23 -0800

    Create Add To Cart button with cookies
    See: #13

--- frontend/src/components/store/single.js
+++ frontend/src/components/store/single.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { withCookies } from 'react-cookie';
 
-
-export default class Single extends Component {
+class Single extends Component {
     constructor(props) {
         super(props);
         this.state = { productId: this.props.location.hash.split('#').pop(),
                        productInfo: {} };
+        this.addToCart = this.addToCart.bind(this);
+            
      }
 
+    addToCart() {
+        const cookies = this.props.cookies;
+        let itemsInCart = cookies.get('userCart');
+
+        if (itemsInCart) {
+            itemsInCart.push(this.state.productId);
+            cookies.set('userCart', itemsInCart , {path: '/'});
+
+        } else {
+            console.log('no cookies exist yet')
+            cookies.set('userCart', [this.state.productId], {path: '/'})
+        }
+        console.log('Current Items', cookies.get('userCart'));
+
+
+    }
 
     componentDidMount() {
 
@@ -35,7 +53,8 @@ export default class Single extends Component {
                     <div className='fake-image'></div>
                     <h2>price: $ {this.state.productInfo.price}</h2>
                     <h3>{this.state.productInfo.description}</h3>
-                    <button>Add To Cart</button>
+                    <p>Hello</p>
+                    <button onClick={() => this.addToCart()}>Add To Cart</button>
 
                 </article>
             </main>
@@ -44,3 +63,9 @@ export default class Single extends Component {
     }
 }
 
+export default withCookies(Single);
+
+/*
+,
+                       user: this.props.cookies.get('user') || ''
+*/
